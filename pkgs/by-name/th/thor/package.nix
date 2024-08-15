91c17a95517b25ae49b5227aed764fe630c5a737
xeals <dev@xeal.me>
2024-08-15 15:07:47 +1000

    thor: init at 1.0.4

---
+++ pkgs/by-name/th/thor/package.nix
@@ -0,0 +1,34 @@
+{ lib
+, fetchFromGitHub
+, buildDotnetModule
+, dotnetCorePackages
+}:
+
+buildDotnetModule rec {
+  pname = "thor";
+  version = "1.0.4";
+
+  src = fetchFromGitHub {
+    owner = "Samsung-Loki";
+    repo = "Thor";
+    rev = version;
+    hash = "sha256-tYzPpgbM9rCDdLW0ERZWmmxzMYpe1BNyFwmpaLQXRGQ=";
+  };
+
+  dotnet-sdk = dotnetCorePackages.sdk_7_0;
+  dotnet-runtime = dotnetCorePackages.runtime_7_0;
+  nugetDeps = ./deps.nix;
+  projectFile = "TheAirBlow.Thor.Shell/TheAirBlow.Thor.Shell.csproj";
+
+  postFixup = ''
+    mv $out/bin/TheAirBlow.Thor.Shell $out/bin/thor
+  '';
+
+  meta = {
+    homepage = "https://github.com/Samsung-Loki/Thor";
+    description = "An alternative to Heimdall";
+    license = lib.licenses.mpl20;
+    platforms = lib.platforms.linux;
+    maintainers = with lib.maintainers; [ xeals ];
+  };
+}
